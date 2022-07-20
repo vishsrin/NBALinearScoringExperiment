eb1a32928366e56bc7084c2d30ab8e9c42f802f3
Vishnu Srinivas <vishnu.8913@gmail.com>
2022-07-19 22:31:02 -0500

    can now do individual player pts  per shot calcs

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ import json
 
 from nba_api.stats.endpoints import shotchartdetail
 
+dunk_score = 2;
+points_per_foot = 0.045;
+
 response = shotchartdetail.ShotChartDetail(
 	context_measure_simple='FGA',
 	team_id=0,
-	player_id=201935,
+	player_id=201566,
 	season_nullable='2021-22',
 	season_type_all_star='Regular Season'
 )
@@ -27,9 +30,26 @@ df.columns = headers
 
 print(headers)
 
-# Iterate over columns using DataFrame.iteritems()
-for (colname,colval) in df.iteritems():
-    print(colname, colval.values)
+df = df.reset_index()  # make sure indexes pair with number of rows
+
+number_shots = 0
+total_score = 0
+for index, row in df.iterrows():
+	print(row['SHOT_DISTANCE'], row['SHOT_TYPE'], row['SHOT_MADE_FLAG'])
+	number_shots = number_shots + 1
+	# total_score = total_score + (1 * row['SHOT_MADE_FLAG'])
+	total_score = total_score + ((dunk_score + row['SHOT_DISTANCE'] * points_per_foot) * row['SHOT_MADE_FLAG'])
+
+points_per_shot = total_score / number_shots
+print(number_shots)
+print(points_per_shot)
+
+
+# # Iterate over columns using DataFrame.iteritems()
+# for (colname,colval) in df.iteritems():
+# 	if (colname == "SHOT_DISTANCE"):
+# 		print(colname, colval.values)
+
 
 # write to csv file
 # df.to_csv( "jamesharden", index=False)
